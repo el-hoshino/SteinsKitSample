ad29bf27532a8b5caf160e29360108248fa458d1
星野恵瑠 <el.hoshino@me.com>
2019-04-04 14:01:25 +0900

    Init Xcode project

---
+++ SteinsKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SteinsKitSample
+//
+//  Created by 史 翔新 on 2019/04/04.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SteinsKitSampleTests/SteinsKitSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SteinsKitSampleTests.swift
+//  SteinsKitSampleTests
+//
+//  Created by 史 翔新 on 2019/04/04.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import XCTest
+@testable import SteinsKitSample
+
+class SteinsKitSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
